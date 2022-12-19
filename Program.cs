b1cebf182eb56d02f40e65c5a71f221a6340672b
Guillermo Barcia <guillermo@pdg.gi>
2022-12-19 10:43:56 +0100

    Add source code

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using QuickSortAlgorithm;
+using System.Globalization;
+
+Console.WriteLine("Welcome to the QuickSortAlgorithm");
+
+List<float> numbersToSort = new();
+bool sortNumbers = false;
+
+while (!sortNumbers)
+{
+    Console.Write("Introduce a number or S to Sort: ");
+    string? input = Console.ReadLine();
+
+    if (input is not null && input.ToLower() == "s") sortNumbers = true;
+
+    // Decimal numbers separated with "," not allowed
+    if (float.TryParse(input, NumberStyles.Float, CultureInfo.InvariantCulture,  out float number))
+    {
+        numbersToSort.Add(number);
+        if (numbersToSort.Count >= 10) sortNumbers = true;
+    }
+    else
+    {
+        Console.WriteLine("Not a valid number");
+    }
+}
+
+Console.WriteLine("This is your list of numbers:");
+
+QuickSort.PrintArray(numbersToSort);
+
+QuickSort.SortArray(numbersToSort, 0, numbersToSort.Count - 1);
+
+Console.WriteLine("This is your list of numbers sorted:");
+
+QuickSort.PrintArray(numbersToSort);
+
+Console.Write("Press any key to exit the process...");
+Console.ReadKey(false);
---
+++ QuickSort.cs
@@ -0,0 +1,57 @@
+﻿namespace QuickSortAlgorithm;
+public static class QuickSort
+{
+    private static int GetPartitionIdx(List<float> arrToSort, int startIdx, int endIdx)
+    {
+        // Get last element as pivot
+        float pivot = arrToSort[endIdx];
+
+        // Get first element index
+        int firstElementIdx = startIdx;
+
+        for (int i = startIdx; i <= endIdx; i++)
+        {
+            if (arrToSort[i] < pivot)
+            {
+                Swap(arrToSort, firstElementIdx, i);
+                firstElementIdx++;
+            }
+        }
+
+        Swap(arrToSort, firstElementIdx, endIdx);
+
+        return firstElementIdx;
+    }
+
+    private static void Swap(List<float> numberList, int firstIdx, int secondIdx)
+    {
+        (numberList[secondIdx], numberList[firstIdx]) = (numberList[firstIdx], numberList[secondIdx]);
+    }
+
+    public static void PrintArray(List<float> arrToPrint)
+    {
+        string result = "";
+        for (int i = 0; i < arrToPrint.Count; i++)
+        {
+            result += $"{arrToPrint[i]}, ";
+        }
+
+        Console.WriteLine(result);
+    }
+
+    public static void SortArray(List<float> arrToSort, int startIdx, int endIdx)
+    {
+        if (startIdx < endIdx)
+        {
+            // Get the patition index of the array
+            int partitionIdx = GetPartitionIdx(arrToSort, startIdx, endIdx);
+
+            // Sort elements before the partition
+            SortArray(arrToSort, startIdx, partitionIdx - 1);
+
+            // Sort elements after the partition
+            SortArray(arrToSort, partitionIdx + 1, endIdx);
+        }
+    }
+}
+
